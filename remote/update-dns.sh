32d767bcb85de70a15f740e3275492ce1f23746c
Tsutomu Kawamura <cognitom@gmail.com>
2017-11-25 10:35:38 +0000

    Add a script which updates Cloud DNS

---
+++ remote/update-dns.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Settings
+ZONE=localhost.zone
+ZONENAME=localhost-zone
+
+# Get the hostname of the instance
+HOSTNAME=$(hostname)
+
+# Get the ip address which is used last time
+LAST_ADDRESS=$(host "${HOSTNAME}.${ZONE}." | sed -rn 's@^.* has address @@p')
+
+# Get the current ip address via Metadata API
+API_ROOT="http://metadata.google.internal/computeMetadata/v1"
+COMMAND="instance/network-interfaces/0/access-configs/0/external-ip"
+CURRENT_ADDRESS=$(curl "${API_ROOT}/${COMMAND}" -H "Metadata-Flavor: Google")
+
+# Update Cloud DNS
+gcloud dns record-sets transaction start -z "${ZONENAME}"
+gcloud dns record-sets transaction remove -z "${ZONENAME}" \
+  --name "${HOSTNAME}.${ZONE}." --ttl 300 --type A "$LAST_ADDRESS"
+gcloud dns record-sets transaction add -z "${ZONENAME}" \
+  --name "${HOSTNAME}.${ZONE}." --ttl 300 --type A "$CURRENT_ADDRESS"
+gcloud dns record-sets transaction execute -z "${ZONENAME}"
