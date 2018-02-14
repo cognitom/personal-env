f4420991e1de25509e546bd479e44b3e54f94980
Tsutomu Kawamura <cognitom@gmail.com>
2018-02-14 15:53:34 +0900

    Update create.sh

--- remote/create.sh
+++ remote/create.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Settings
-PROJECT_NAME="personal-env"
+PROJECT_NAME="librize-dev"
 GITHUB_REPO="cognitom/personal-env"
-
+GITHUB_REPO_BRANCH="librize"
 
 # Arguments
 INSTANCE_NAME="$1"
@@ -16,7 +16,7 @@ fi
 
 # Download startup script
 TEMP=$(mktemp -u)
-CONTENTS_ROOT="https://raw.githubusercontent.com/${GITHUB_REPO}/master"
+CONTENTS_ROOT="https://raw.githubusercontent.com/${GITHUB_REPO}/${GITHUB_REPO_BRACNH}"
 curl ${CONTENTS_ROOT}/remote/startup-script.sh > "${TEMP}"
 
 # Get Service Account information
